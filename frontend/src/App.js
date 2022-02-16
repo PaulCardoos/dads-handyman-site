9a5e4e350fd62808453f8d1376d948a2c8973655
PaulCardoos <paulcardoos@gmail.com>
2022-02-15 19:21:36 -0500

    inital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes, 
+  Route
+} from "react-router-dom"
+import Navigation from './components/Navigation';
+import Home from "./screens/Home"
+import Services from './screens/Services';
+import Contact from "./screens/Contact"
+import Reason from './screens/Reason';
+import RequestService from './screens/RequestService';
+import About from './screens/About';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/faq" element={<Reason/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/service-request" element={<RequestService/>}/>
+        <Route path="/about" element={<About/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/Services.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container} from "react-bootstrap"
+const Services = () => {
+  return (
+    <Container>
+      <h1 className="text-start" style={{margin:"15px"}}>
+        Services
+
+
+      </h1>
+
+
+    </Container>
+  ) 
+}
+
+export default Services
